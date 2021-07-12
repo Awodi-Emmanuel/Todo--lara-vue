923819622df163bf7e3d5ad5b3ca6b4bb07e7ed2
emmy70 <emmanuelusman701@gmail.com>
2021-07-12 01:06:30 +0100

    completed my laravel vue todo-app

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,17 @@ import Vue from "vue";
 
 import App from "./vue/app";
 
+
+ // fontawesome needs installation
+ import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+ library.add( faPlusSquare, faTrash )
+
+ Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 const app = new Vue({
     el: "#app",
     components: { App },
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+       <div v-for="(items, index) in items" :key="index">
+           <list-item
+                :item="item"
+                class="item"
+                v-on:itemChanged="$emit('reloadlist ')"
+                />
+       </div>
+    </div>
+</template>
+
+
+<script>
+// import { component } from 'vue/types/umd'
+import listItem from "./listItem"
+import ListItem from './listItem.vue'
+
+export default {
+  components: { listItem },
+    props: ['items'],
+    components:{
+         listItem
+
+    }
+}
+</script>
+
+<style scoped>
+.item {
+    background: #e6e6e6;
+    padding: 5px;
+    margin-top: 5px;
+}
+
+</style>
